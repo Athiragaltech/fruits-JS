8602ea5db2c37f61903000eb260064997c1cb6c0
Athira <athiranass@gmail.com>
2024-03-01 10:39:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function organizeFruits() 
+{
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
+let appleShelf = document.getElementById("apple-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+    for (let i = 0; i <= fruit.length; i++)
+    {
+        if (fruit[i] === "🍎")
+        {
+            appleShelf.textContent += "🍎 ";
+           
+
+        }
+        else if (fruit[i] === "🍊")
+        {
+            orangeShelf.textContent += "🍊 ";
+        }
+    }
+}
